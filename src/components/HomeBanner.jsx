2d976c155921216921238686c363ab17112f0239
dhanushramudri_jman <dhanush.r@jmangroup.com>
2025-02-05 17:22:17 +0530

    added responsive

--- src/components/HomeBanner.jsx
+++ src/components/HomeBanner.jsx
@@ -6,7 +6,7 @@ const HomeBanner = () => {
       <h1 className="text-4xl md:text-6xl font-bold mb-4 bg-gradient-to-r from-cyan-500 py-4 to-blue-600 text-transparent bg-clip-text">
         Welcome to <span className="text-white">My</span><span className="bg-gradient-to-r from-cyan-500 py-4 to-blue-600 text-transparent bg-clip-text">Blog</span>
       </h1>
-      <p className="text-xl md:text-2xl text-gray-400 font-bold  text-white  ">
+      <p className="text-xl md:text-2xl text-gray-400 font-bold  text-white  max-sm:text-center  ">
         A place where you can share your thoughts and ideas with the world.
       </p>
     </div>
